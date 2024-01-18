637b9cca07386475324de336183a3e214224e75c
[TheoXenius] <[theo.bedu@icloud.com]>
2024-01-18 11:44:46 +0100

    Mini-projet-laravel-revision

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Article;
+use App\Models\Image;
+use Illuminate\Http\Request;
+
+class MainController extends Controller
+{
+    public function home() {
+        //$articles = Article::all();
+        $articles = Article::orderBy('titre')->get();
+        $article11 = Article::find(11);
+        
+
+        return view('home', [
+            'articles' => $articles,
+            'article11' => $article11,
+        ]);
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,32 @@
+@extends('base')
+
+@section('title', 'Accueil')
+
+@section('content')
+<h1>Accueil</h1>
+<p>
+    Vous êtes sur la page d'accueil.
+</p>
+
+<h2>{{ $article11->titre }}</h2>
+<p>{{ $article11->texte }}</p>
+<ul>
+    @foreach ($article11->images() as $image)
+    <li>
+        <img src="{{ asset('images/'.$image->fichier) }}" alt="{{ $image->titre}}">
+    </li>
+    @endforeach
+</ul>
+
+@foreach ($articles as $article)
+<h2>{{ $article->titre }}</h2>
+<p>{{ $article->texte }}</p>
+<ul>
+    @foreach ($article->images() as $image)
+    <li>
+        <img src="{{ asset('images/'.$image->fichier) }}" alt="{{ $image->titre}}">
+    </li>
+    @endforeach
+</ul>
+@endforeach
+@endsection
